0fc10563e43c58922f19b10ac581873ce52eef3a
bakka22 <abobakereltom@gmail.com>
2024-08-09 01:30:02 +0200

    s

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- ranks county origins of bands orderd by the number of fans
-SELECT origin, COUNT(fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 from metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
