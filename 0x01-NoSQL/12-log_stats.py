acc55164eb99dcd0cbf3976a069a3d1ba487284f
bakka22 <abobakereltom@gmail.com>
2024-08-14 01:54:08 +0200

    s

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,23 +2,23 @@
 """ nginx logs """
 from pymongo import MongoClient
 
-
-reqs = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-client = MongoClient()
-db = client.logs
-collection = db.nginx
-print(f"{collection.count_documents({})} logs")
-pipeline = [
-    {'$match': {'method': {'$in': reqs}}},
-    {'$group': {'_id': '$method', 'sum': {'$sum': 1}}}
-]
-print("Methods:")
-result = collection.aggregate(pipeline)
-for doc in result:
-    if doc['_id'] in reqs:
-        print(f"\tmethod {doc['_id']}: {doc['sum']}")
-        reqs.remove(doc['_id'])
-for req in reqs:
-    print(f"\tmethod {req}: {0}")
-st_checks = collection.count_documents({"method": 'GET', "path": "/status"})
-print(f"{st_checks} status check")
+if __name__ == "__main__":
+    reqs = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
+    print(f"{collection.count_documents({})} logs")
+    pipeline = [
+        {'$match': {'method': {'$in': reqs}}},
+        {'$group': {'_id': '$method', 'sum': {'$sum': 1}}}
+    ]
+    print("Methods:")
+    result = collection.aggregate(pipeline)
+    for doc in result:
+        if doc['_id'] in reqs:
+            print(f"\tmethod {doc['_id']}: {doc['sum']}")
+            reqs.remove(doc['_id'])
+    for req in reqs:
+        print(f"\tmethod {req}: {0}")
+    st_checks = collection.count_documents({"method": 'GET', "path": "/status"})
+    print(f"{st_checks} status check")
