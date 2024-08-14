6ba7ced0a3c97315fc2f5e7cf4f7bef07efd331d
bakka22 <abobakereltom@gmail.com>
2024-08-14 19:56:11 +0200

    s

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,27 +12,46 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ store data using a random key """
+"""    def store(self, data: Union[str, bytes, int, float]) -> str:
+        "" store data using a random key ""
         uid = str(uuid.uuid4())
         self._redis.set(uid, data)
         return uid
-
     def get(self, key: str, fn: Callable = None) ->
             Union[str, bytes, int, float]:
-        """ get the value from redis based on key
-        and convert it to desired format """
+        "" get the value from redis based on key
+        and convert it to desired format ""
         value = self._redis.get(key)
         if fn:
             value = fn(value)
         return value
 
     def get_str(self, key: str) -> str:
-        """ get the value in string format
-        from redis based on key"""
+        "" get the value in string format
+        from redis based on key""
         return self.get(key, lambda d: d.decode("utf-8"))
 
     def get_int(self, key: str) -> str:
-        """ get the value in int format
-        from redis based on key"""
+        "" get the value in int format
+        from redis based on key""
+        return self.get(key, lambda x: int(x))
+"""
+    def get(
+            self,
+            key: str,
+            fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        '''Retrieves a value from a Redis data storage.
+        '''
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        '''Retrieves a string value from a Redis data storage.
+        '''
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        '''Retrieves an integer value from a Redis data storage.
+        '''
         return self.get(key, lambda x: int(x))
