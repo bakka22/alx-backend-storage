e425b148bb1abd94a4de907c30ed1f10e4f95d7e
bakka22 <abobakereltom@gmail.com>
2024-08-14 21:10:50 +0200

    s

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,15 @@
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def my_decorator(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        args[0]._redis.incr(f.__qualname__, 1)
+        return f(*args, **kwargs)
+    return wrapper
 
 
 class Cache():
@@ -12,15 +21,18 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @my_decorator
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ store data using a random key """
         uid = str(uuid.uuid4())
         self._redis.set(uid, data)
         return uid
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+
+    def get(self, key: str, fn: Optional[Callable] =
+            None) -> Union[str, bytes, int, float]:
         """ get the value from redis based on key
         and convert it to desired format """
-        value = redis.Redis().get(key)
+        value = self._redis.get(key)
         if fn and value:
             value = fn(value)
         return value
